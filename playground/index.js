40ff18c9deae1ad05a508c46b6cc9b43ce187b0c
Roland Schenk <roland.schenk@hoermoos.ch>
2021-10-11 15:43:26 +0200

    initit again

---
+++ playground/index.js
@@ -0,0 +1,7 @@
+import React from "https://unpkg.com/react@16/umd/react.development.js";
+import ReactDOM from "https://unpkg.com/react-dom@16/umd/react-dom.development.js";
+
+
+
+
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
